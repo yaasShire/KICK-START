0fd6665ad6f3a85b35530492a820d37226c957a2
yusuf <yusufshire58@gmail.com>
2024-11-03 14:30:57 +0300

    lll

--- app/api/baseURL.js
+++ app/api/baseURL.js
@@ -1,2 +1,2 @@
-// export const baseURL = "https://spondias.com/sporton/api/v1/";
-export const baseURL = "http://172.20.10.7:8089/api/v1/";
+export const baseURL = "https://spondias.com/sporton/api/v1/";
+// export const baseURL = "http://172.20.10.7:8089/api/v1/";
